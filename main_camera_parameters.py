8d7b016a7bf9bb7aedd0d0df56472a5df250272f
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-11-27 09:48:16 -0800

    Update main_camera_parameters.py

--- main_camera_parameters.py
+++ main_camera_parameters.py
@@ -1,1 +1,22 @@
+from src.image_extractor import extract_images
+
+import numpy as np
+
+def main():
+
+  # Specify the video name
+  video_file = 'data/videos/checkerboard.mp4'
+  output_dir = 'data/checkerboard'
+
+  # Extract the frames to data/images
+  extract_images(video_file, output_dir, frame_interval=1)
+
+  # Run the camera parameter algorithm
+  camera_parameters = process_checkerboards(output_dir)
+
+  # Save the camera parameters
+  np.savetxt(camera_parameters)
+
+if __name__ == '__main__':
+  main()
 
