598cd92b6ddd5a49c9705099cc038d42baf597ae
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-11-27 09:40:59 -0800

    Update main_video_to_images.py

--- main_video_to_images.py
+++ main_video_to_images.py
@@ -1,13 +1,13 @@
-from src.image_extract import extract_frames
+from src.image_extract import extract_images
 
 def main():
 
   # Specify the video name
-  video_filename = 'data/videos/test.MOV'
-  image_output_path = 'data/images'
+  video_file = 'data/videos/test.MOV'
+  output_dir = 'data/images'
 
   # Extract the frames to data/images
-  extract_frames(video_filename, image_output_path)
+  extract_images(video_file, output_dir, frame_interval=1)
 
 if __name__ == '__main__':
   main()
