8021446085669fb8553fb7e18f5148ac75fd97f7
RelaxingDistress <mvijla23@gmail.com>
2024-12-27 05:37:19 +0530

    Update main_video_to_images.py

--- main_video_to_images.py
+++ main_video_to_images.py
@@ -3,11 +3,12 @@ from src.image_extractor import extract_images
 def main():
 
   # Specify the video name
-  video_file = 'data/videos/dog_video.mp4'
-  output_dir = 'data/images'
+  video_name = 'dog_video'
+  video_file = f'data/videos/{video_name}.mp4'
+  output_dir = f'data/{video_name}/rgb'
 
   # Extract the frames to data/images
-  extract_images(video_file, output_dir, frame_interval=1)
+  extract_images(video_file, output_dir, frame_interval=10)
 
 if __name__ == '__main__':
   main()
